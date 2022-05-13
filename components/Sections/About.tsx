ca4300c1c5e12a80e4e3c5e5b670a455da005f75
Dhruva Srinivas <dhruvas17068@gmail.com>
2022-05-13 16:33:24 +0530

    chore: format

--- components/Sections/About.tsx
+++ components/Sections/About.tsx
@@ -3,7 +3,7 @@ import type { NextComponentType } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
 import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 import { VscGithubAlt } from "react-icons/vsc";
@@ -29,21 +29,22 @@ const About: NextComponentType = () => {
           Developer, Blogger, Student, Software Enthusiast
         </p>
 
-        <p className="mt-4 text-gray-400 pb-5">
+        <p className="mt-4 pb-5 text-gray-400">
           I like to build web applications, write blog posts on tech and <br /> explore programming.
           Apart from coding, I like to read, draw and play sports.
         </p>
 
         <Link href="https://blog.dhruva.is-a.dev" passHref>
-          <motion.div whileHover={{
-            scale: 1.025
-          }}>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
+          <motion.div
+            whileHover={{
+              scale: 1.025,
+            }}
           >
-            <button className="rounded-lg px-5 py-2 bg-yellow-300 hover:bg-yellow-400 flex flex-row">Blog <HiOutlineArrowNarrowRight className="pl-1" size={24}/></button>
-          </a>
+            <a target="_blank" rel="noopener noreferrer">
+              <button className="flex flex-row rounded-lg bg-yellow-300 px-5 py-2 hover:bg-yellow-400">
+                Blog <HiOutlineArrowNarrowRight className="pl-1" size={24} />
+              </button>
+            </a>
           </motion.div>
         </Link>
       </div>
