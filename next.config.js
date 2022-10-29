c0e393758ca767696065501decb5946b69249c3b
Dhruva Srinivas <dhruvas17068@gmail.com>
2022-10-29 15:54:21 +0530

    fix: test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 
 module.exports = {
+  experimental: {
+    runtime: "experimental-edge",
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
