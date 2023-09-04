d55f16a4806e8fda98c151a1549cc9a84f718f69
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-09-03 20:26:55 -0700

    projects

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,55 +1,70 @@
 import { IProject } from "../types/Project";
 
 export const GRADIENTS: string[] = [
-  "#D8B4FE, #818CF8",
-  "#bfdbfe, #6366f1, #6d28d9",
-  "#86efac, #d8b4fe, #818cf8",
-  "#f8b4fe, #818cf8, #d8b4fe",
-  "#bbf7d0, #4ade80, #22c55e",
-  "#ca8a04, #dc2626",
-  "#99f6e4, #d9f99d",
-  "#eab308, #a855f7, #3b82f6",
+	"#D8B4FE, #818CF8",
+	"#bfdbfe, #6366f1, #6d28d9",
+	"#86efac, #d8b4fe, #818cf8",
+	"#f8b4fe, #818cf8, #d8b4fe",
+	"#bbf7d0, #4ade80, #22c55e",
+	"#ca8a04, #dc2626",
+	"#99f6e4, #d9f99d",
+	"#eab308, #a855f7, #3b82f6",
 ];
 
 export const PROJECTS: IProject[] = [
-  {
-    name: "Cubedeck",
-    desc: "Taking your cubing skills to the moon!",
-    href: "https://cubedeck.netlify.app",
-  },
-  {
-    name: "rettiwt",
-    desc: "twitter without the fluff",
-    href: "https://rettiwtt.vercel.app",
-  },
-  {
-    name: "env-lock",
-    desc: "A CLI to manage your .env files.",
-    href: "https://github.com/carrotfarmer/env-lock",
-  },
-  {
-    name: "sz",
-    desc: "Easy-to-use disk usage CLI.",
-    href: "https://github.com/carrotfarmer/sz",
-  },
-  {
-    name: "readist",
-    desc: "the easiest way to track your reading",
-    href: "https://readist.vercel.app",
-  },
-  {
-    name: "Taskboard",
-    desc: "Todoist extension for your browser homepage.",
-    href: "https://github.com/carrotfarmer/taskboard",
-  },
-  {
-    name: "kitty.js",
-    desc: "A JavaScript wrapper for TheCatAPI",
-    href: "https://npmjs.com/package/@kittyjs/kitty-js",
-  },
-  {
-    name: "snake",
-    desc: "snake",
-    href: "https://github.com/carrotfarmer/snake",
-  },
+	{
+		name: "Cubedeck",
+		desc: "Taking your cubing skills to the moon!",
+		href: "https://cubedeck.netlify.app",
+	},
+	{
+		name: "rettiwt",
+		desc: "twitter without the fluff",
+		href: "https://rettiwtt.vercel.app",
+	},
+	{
+		name: "env-lock",
+		desc: "A CLI to manage your .env files.",
+		href: "https://github.com/carrotfarmer/env-lock",
+	},
+	{
+		name: "sz",
+		desc: "Easy-to-use disk usage CLI.",
+		href: "https://github.com/carrotfarmer/sz",
+	},
+	{
+		name: "readist",
+		desc: "the easiest way to track your reading",
+		href: "https://readist.vercel.app",
+	},
+	{
+		name: "Taskboard",
+		desc: "Todoist extension for your browser homepage.",
+		href: "https://github.com/carrotfarmer/taskboard",
+	},
+	{
+		name: "kitty.js",
+		desc: "A JavaScript wrapper for TheCatAPI",
+		href: "https://npmjs.com/package/@kittyjs/kitty-js",
+	},
+	{
+		name: "snake",
+		desc: "snake",
+		href: "https://github.com/carrotfarmer/snake",
+	},
+	{
+		name: "chttp",
+		desc: "HTTP Server/Library in C++ from scratch",
+		href: "https://github.com/carrotfarmer/chttp",
+	},
+	{
+		name: "warlock",
+		desc: "easy and secure password management",
+		href: "https://github.com/carrotfarmer/warlock",
+	},
+	{
+		name: "statspend",
+		desc: "Expense Tracker",
+		href: "https://github.com/carrotfarmer/statspend",
+	},
 ];
